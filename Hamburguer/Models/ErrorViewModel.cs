65c207b5ed40d9a01056912cfadd9318361b0b7d
Vivian Freitas <freitasvivian2001@gmail.com>
2024-08-08 21:36:07 -0300

    hamburguer

---
+++ Hamburguer/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Hamburguer.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
